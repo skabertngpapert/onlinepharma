cfa4e44f5f6ec05c9ee0281adc98e118e6283c7b
skabertngpapert <122263400+skabertngpapert@users.noreply.github.com>
2023-01-09 18:00:49 +0800

    connection

--- database/connection.php
+++ database/connection.php
@@ -1,10 +1,10 @@
 <?php
 function OpenCon()
 {
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$db = "pharmacy";
+$dbhost = "sql6.freemysqlhosting.net";
+$dbuser = "sql6589257";
+$dbpass = "6RLihjnGxe";
+$db = "sql6589257";
 $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
 return $conn;
